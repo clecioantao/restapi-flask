911168b574efff968da48476261a5e330c8ca323
Clecio Antao <clecio.antao@gmail.com>
2024-07-21 21:12:39 -0300

    fix: position corrections

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'username': 'admin', 
+    'password': 'admin'
+}
+
 api = Api(app)
+db = MongoEngine(app)
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
+    
 
 class Users(Resource):
     def get(self):
-        return {'message': 'users'}
+        # return {'message': 'users'}
+        return jsonify(UserModel.objects.all())
 
 class User(Resource):
     def post(self):
